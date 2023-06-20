119dfc3d2fab07af67cb8e5fb0753ec1af3dd256
alphajedidia <alphajedidia01@gmail.com>
2023-06-20 07:42:38 +0300

    ok

---
+++ js/app.js
@@ -0,0 +1,18 @@
+const next = document.getElementsByClassName("nextQuestion")
+const frame = document.getElementsByClassName("frame")
+const result = document.getElementById('result')
+for(let x in next){
+    next[x].addEventListener("click",()=>{
+        if(x == 15){
+            console.log("ok")
+            
+        }
+        else{
+            let y = 1
+            console.log(x)
+            frame[y+parseInt(x)].style.display = 'block'
+            frame[x].style.display = 'none'
+        }
+    })
+
+}
